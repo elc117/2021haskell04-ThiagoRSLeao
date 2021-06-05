92d24d6adae7ad36ef7823a49f07cf1c0f56dd61
Thiago Leão <>
2021-06-05 13:41:09 +0000

    Made a cute meteor!

--- svgPatterns.hs
+++ svgPatterns.hs
@@ -43,9 +43,17 @@ svgRect :: Rect -> String -> String
 svgRect ((x,y),w,h) style = 
   printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h style
 
+svgCirc :: Circle -> String -> String 
+svgCirc ((x,y),r) style = 
+  printf "<circle cx='%.3f' cy='%.3f' r='%.2f' style='%s' />\n" x y r style
+
+svgPath :: String -> String -> String 
+svgPath d style = 
+  printf "<path d='%s' style='%s' />\n" d style
+  
 -- String inicial do SVG
 svgBegin :: Float -> Float -> String
-svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
+svgBegin w h = printf "<svg width='%.2f' height='%.2f' style='background: white;transform: rotate(30deg);' xmlns='http://www.w3.org/2000/svg'>\n" w h 
 
 -- String final do SVG
 svgEnd :: String
@@ -61,19 +69,22 @@ svgStyle (r,g,b) = printf "fill:rgb(%d,%d,%d); mix-blend-mode: screen;" r g b
 svgElements :: (a -> String -> String) -> [a] -> [String] -> String
 svgElements func elements styles = concat $ zipWith func elements styles
 
+
+cx ((x,y),r) = x;
+cy ((x,y),r) = y;
+cr ((x,y),r) = r;
+
 -------------------------------------------------------------------------------
 -- Função principal que gera arquivo com imagem SVG
 -------------------------------------------------------------------------------
 
+
 main :: IO ()
 main = do
-  writeFile "rects.svg" $ svgstrs
+  writeFile "figs.svg" $ svgstrs
   where svgstrs = svgBegin w h ++ svgfigs ++ svgEnd
-        svgfigs = svgElements svgRect rects (map svgStyle palette)
-        rects = genRectsInLine nrects
-        palette = rgbPalette nrects
-        nrects = 10
-        (w,h) = (1500,500) -- width,height da imagem SVG
+        svgfigs =  svgPath "M 225 200 C 200 150 275 100 250 10" "stroke: rgb(255, 102, 36); stroke-width: 6; fill: none;" ++ svgPath "M 249 10 C 325 150 200 100 275 200" "stroke: rgb(255, 102, 36); stroke-width: 6; fill: none;" ++ svgPath "M 275 250 C 250 200 325 150 300 60" "stroke: rgb(255, 102, 36); stroke-width: 6; fill: none;" ++ svgPath "M 299 60 C 375 200 250 150 325 250" "stroke: rgb(255, 102, 36); stroke-width: 6; fill: none;" ++ svgPath "M 225 250 C 225 150 150 150 200 60" "stroke: rgb(255, 102, 36); stroke-width: 6; fill: none;" ++ svgPath "M 199 60 C 100 150 225 150 175 250" "stroke: rgb(255, 102, 36); stroke-width: 6; fill: none;" ++ svgCirc ((250,250), 75) "fill:rgb(255, 47, 36);stroke: rgb(235, 16, 5);stroke-width: 6;"
+        (w,h) = (500,500) -- width,height da imagem SVG
 
 
 
